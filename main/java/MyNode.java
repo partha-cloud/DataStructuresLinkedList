c76f263c3add0e899d06614b53b46c2001cfa7a9
partha-cloud <partha11dey@gmail.com>
2021-04-06 22:01:52 +0530

    added

---
+++ main/java/MyNode.java
@@ -0,0 +1,17 @@
+public class MyNode<K> {
+    private MyNode next;
+    private K key;
+
+    public MyNode getNext() {
+        return next;
+    }
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+    public MyNode(K key) {
+        this.key = key;
+        this.next = next;
+    }
+
+}
+
